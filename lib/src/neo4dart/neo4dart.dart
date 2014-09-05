a6ec8b5609a26992d8864b18d10956fec2d41048
Matthieu MAQUEVICE <matthieu.maquevice@numergy.com>
2014-09-05 17:41:21 +0200

    repo init

--- lib/src/neo4dart/neo4dart.dart
+++ lib/src/neo4dart/neo4dart.dart
@@ -8,12 +8,4 @@
 import 'dart:io';
 
 main() {
-  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 4040)
-      .then((HttpServer server) {
-    print('listening on localhost, port ${server.port}');
-    server.listen((HttpRequest request) {
-      request.response.write('Hello, world!');
-      request.response.close();
-    });
-  }).catchError((e) => print(e.toString()));
 }
